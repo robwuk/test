8b5c1aa30c1188e16835806411091a09ebd41be8
Robert Worth <robertworth@hotmail.com>
2019-01-22 11:07:00 +0000

    build

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-//  res.send("<h1>Login</h1><br />");
   res.render('form', { title: 'Registration form' });
 });
 
